2ada8fa30c6b40fdbd0b662d8edd1642895736ca
Amila Welihinda <amilajack@gmail.com>
2017-03-17 15:25:22 -0700

    Initial working commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const {db, config} = require('sqlectron-core');
+
+const serverInfo = {
+  database: path.join(__dirname, 'sqlectron.sqlite'),
+  client: 'sqlite'
+};
+
+async function main() {
+    const serverSession = db.createServer(serverInfo);
+    const connection = await serverSession.createConnection('compat-db');
+    await connection.connect(serverInfo)
+    console.log(await connection.listTables())
+    console.log(await connection.listDatabases())
+}
+
+main();
+
